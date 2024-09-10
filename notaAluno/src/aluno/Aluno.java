dd3c9b57e2af7528099712546bc18101d7d5451c
Filipe Santos <143547860+Filipe-S-Silva@users.noreply.github.com>
2024-09-10 20:55:21 -0300

    Update Aluno.java

--- notaAluno/src/aluno/Aluno.java
+++ notaAluno/src/aluno/Aluno.java
@@ -2,18 +2,19 @@ package aluno;
 
 public class Aluno {
 
-    public double media;
+    public double soma;
     public String nome;
     public double[] notas = new double[3];
-    public double calcularMedia(double soma){
-     return media = soma / 3;
+    public String resultado;
+    public double faltaPonto;
+    public void conseguirResultado(double soma){
+        if (soma >= 60){
+            resultado = "Aprovado";
+        } else {
+            resultado = "Reprovado";
+            faltaPonto = 60 - soma;
+
+        }
     };
-
-    public String resultado(double media){
-        
-    };
-
-
-
 
 }
